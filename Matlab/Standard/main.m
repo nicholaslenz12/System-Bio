b2f1ec2cb4aa0eb2f1372e46974fb9d567781851
Nicholas Lenz <nicholaslenz12@gmail.com>
2018-09-19 19:30:23 -0700

    New implementation with pop ratios

--- Matlab/Standard/main.m
+++ Matlab/Standard/main.m
@@ -1,22 +1,22 @@
 %% Initial Conditions 
-WT_0 = 200;
-C_0 = 50;
-A_0 = 0;
-x1 = [WT_0, C_0, A_0, 0];
-x2 = [WT_0, C_0, A_0, 1];
+R_0 = 4; % Ratio of the two populations
+Prop_WT_0 = 1; % Proportion of the WT population present at any time compared to the start
+A_0 = 0; % Antibiotic concentration
+x1 = [R_0 Prop_WT_0 A_0 0];
+x2 = [R_0 Prop_WT_0 A_0 1];
 
 start_time = 0;
-WT_ref     = WT_0;
-solutions  = [start_time, WT_0, C_0, A_0, 0, WT_ref];
-distances  = cast(intmax,'double');
+WT_ref     = Prop_WT_0;
+solutions  = [start_time R_0 Prop_WT_0 A_0 0 WT_ref];
 
 lookahead  = 30;
 recession_length = 10;
 ratio = recession_length/lookahead;
-step_count = 100;
+step_count = 200;
 new_index = cast(step_count*ratio, 'int32');
-rho        = .004;
-bound      = 250;
+rho        = .005;
+bound      = 100;
+distances  = bound + .01;
 
 %% Perform RHC
 
@@ -24,40 +24,38 @@ count = 0;
 
 while count < 1000
     
+    % Generates the timespan for the iteration.
     end_time = start_time + lookahead;
     tspan=start_time:lookahead/step_count:end_time;
+    
+    % Sets the reference for each time in the time span.
     WT_ref_vec = WT_ref.*ones(size(tspan)).';
     
+    % Simulates for \mu = 0 and \mu = 1
     [times1, solutions1] = simulate(x1,tspan,rho);
     [times2, solutions2] = simulate(x2,tspan,rho);
-    distance1 = calculate_distance(WT_ref_vec,solutions1(:,1));
-    distance2 = calculate_distance(WT_ref_vec,solutions2(:,1));
+    distance1 = calculate_distance(WT_ref_vec,solutions1(:,2));
+    distance2 = calculate_distance(WT_ref_vec,solutions2(:,2));
     
     if distance1 <= distance2
-        x1 = [solutions1(new_index+1,1),solutions1(new_index+1,2), ...
-              solutions1(new_index+1,3),0];
-        x2 = [solutions1(new_index+1,1),solutions1(new_index+1,2), ...
-              solutions1(new_index+1,3),1];
+        x1 = [solutions1(new_index+1,1),solutions1(new_index+1,2),solutions1(new_index+1,3),0];
+        x2 = [solutions1(new_index+1,1),solutions1(new_index+1,2),solutions1(new_index+1,3),1];
         solutions = [solutions; tspan(2:new_index+1).', ...
                      solutions1(2:new_index+1,:), WT_ref_vec(2:new_index+1)];
         distances = [distances;distance1];
-        start_time = start_time + 10;
+        start_time = start_time + recession_length;
     else
-        x1 = [solutions2(new_index+1,1),solutions2(new_index+1,2), ...
-              solutions2(new_index+1,3),0];
-        x2 = [solutions2(new_index+1,1),solutions2(new_index+1,2), ...
-              solutions2(new_index+1,3),1];
+        x1 = [solutions2(new_index+1,1),solutions2(new_index+1,2),solutions2(new_index+1,3),0];
+        x2 = [solutions2(new_index+1,1),solutions2(new_index+1,2),solutions2(new_index+1,3),1];
         solutions = [solutions; tspan(2:new_index+1).', ...
                      solutions2(2:new_index+1,:), WT_ref_vec(2:new_index+1)];
         distances = [distances;distance2];
         start_time = start_time + recession_length;
     end
     
-    
     if distances(end) < bound && distances(end-1) < bound
-        WT_ref = .99*WT_ref;
+        WT_ref = .95*WT_ref;
     end
-    
     
     count = count + 1;
 end
@@ -67,14 +65,17 @@ plot(solutions(:,1),(solutions(:,2)),'LineWidth',2,'Color',[1 0 0])
 hold on
 plot(solutions(:,1),(solutions(:,3)),'LineWidth',2,'Color',[0 0 1])
 hold on
-plot(solutions(:,1),(solutions(:,6)),'LineWidth',2,'Color',[1 0.5 0])
+plot(solutions(:,1),.01*(solutions(:,4)),'LineWidth',2,'Color',[0.5 1 0])
 hold on
-plot(solutions(:,1),0.5*C_0*(solutions(:,5)),'LineWidth',1.5,'Color',[0.5 1 0])
+plot(solutions(:,1),(solutions(:,5)),'LineWidth',2,'Color',[0 1 0])
 hold on
-plot(solutions(:,1),(solutions(:,4)),'LineWidth',2,'Color',[0 1 0])
-legend('Invasive Pop.', 'Controller Pop.', 'Target Size', 'State','A')
+plot(solutions(:,1),(solutions(:,6)),'LineWidth',2,'Color',[0 1 1])
+legend('Population Ratios (Wild-Type/Controller)', ...
+       'Wild-Type Proportion (Current/Initial)','Antiobiotic Concentration', ...
+       '\mu','Reference Wild-Type Proportion')
 hold off
-axis([0 1000 0 400])
+
+xlim([0 1000])
 xlabel('Time (minutes)')
 title('Population Dynamics')
 grid on
--- Matlab/Standard/simulate.m
+++ Matlab/Standard/simulate.m
@@ -1,9 +1,9 @@
 function [times, values] = simulate(initial_conditions,tspan,rho)
 %SIMULATE Simulates the ode with given inital conditions over a time span.
 %   Simulate numerically integrates the system of ODEs defined as:
-%   WT' = F(WT, C, A)
-%   C   = G(A)
-%   A   = H(WT, C, A)
+%   R'  = F(R, A)
+%   Pwt = G(R, A)
+%   A'  = G(A)
 
 % values
 rmax = log(2)/20;
@@ -17,16 +17,22 @@ alpha = 1;
 [times, values]=ode45(@growth_control,tspan,initial_conditions);
 
 function dydt = growth_control(t, x)
-    Aeff = x(2)*x(3)/x(1);
     
+    Aeff = x(3)/x(1);
     dydt = zeros(size(x));
-    dydt(1) = rmax*(1 - (Aeff^B1)/(K1^B1 + Aeff^B1))*x(1) ...
-              - (gammawt*Aeff/(K1 + Aeff))*x(1);
+    
+    % The change in the ratio of the populations
     if -rho*x(3) + rmax > 0
-        dydt(2) = (-rho*x(3) + rmax)*x(2);
+        dydt(1) = x(1)*(rho*x(3) - rmax*((Aeff^B1)/(K1^B1 + Aeff^B1)) - gammawt*Aeff/(K1 + Aeff));
     else
-        dydt(2) = 0;
+        dydt(1) = x(1)*(rmax*(1 - (Aeff^B1)/(K1^B1 + Aeff^B1)) - gammawt*Aeff/(K1 + Aeff));
     end
+    
+    % The change in the proportion of the WT present now compared to the
+    % start
+    dydt(2) = rmax*(1 - (Aeff^B1)/(K1^B1 + Aeff^B1)) - gammawt*Aeff/(K1 + Aeff)*x(2);
+    
+    % Change in the antibiotic concentration
     if x(4) == 1
         dydt(3) = alpha - gamma*x(3);
     else
